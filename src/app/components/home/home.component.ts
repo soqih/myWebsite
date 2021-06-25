4b454afcc5ae0c0f4a0b3df02785f3773389ca06
abdullah ibrahim <abo.gba@gmail.com>
2021-06-25 21:53:30 +0300

    aos , and general enhancements

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+// import AOS from 'aos/dist/aos';
 
 @Component({
   selector: 'app-home',
@@ -8,28 +9,9 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  // @ViewChild("hiddenNav") hiddenNav: ElementRef;
-  // @ViewChild("hamburger") hamburger: ElementRef;
-
-  @ViewChild("logos") logos: ElementRef;
-  @ViewChild("logoCard") logoCard: ElementRef;
-  @ViewChild("logoCardText") logoCardText: ElementRef;
-
-  @ViewChild("kufic") kufic: ElementRef;
-  @ViewChild("kuficCard") kuficCard: ElementRef;
-  @ViewChild("kuficCardText") kuficCardText: ElementRef;
-
-  @ViewChild("other") other: ElementRef;
-  @ViewChild("otherCard") otherCard: ElementRef;
-  @ViewChild("otherCardText") otherCardText: ElementRef;
-
-
   constructor(public route: ActivatedRoute) { }
   ngOnInit(): void {
     console.log(this.route.snapshot.routeConfig.path)
-    // if(this.route.snapshot.routeConfig.path.includes("Ar")){
-      
-    // }
   }
 
   
@@ -40,24 +22,5 @@ export class HomeComponent implements OnInit {
     return 'english'
   }
   
-  show(a) {
-    if (a === "logos") {
-      this.logos.nativeElement.classList.toggle('hide');
-      this.logos.nativeElement.scrollIntoView({ behavior: "smooth", block: 'center' });
-      this.logoCard.nativeElement.classList.toggle('clicked');
-      this.logoCardText.nativeElement.classList.toggle('white');
-    }
-    if (a === "kufic") {
-      this.kufic.nativeElement.classList.toggle('hide');
-      this.kufic.nativeElement.scrollIntoView({ behavior: "smooth", block: 'center' });
-      this.kuficCard.nativeElement.classList.toggle('clicked');
-      this.kuficCardText.nativeElement.classList.toggle('white');
-    }
-    if (a === "other") {
-      this.other.nativeElement.classList.toggle('hide');
-      this.other.nativeElement.scrollIntoView({ behavior: "smooth", block: 'center' });
-      this.otherCard.nativeElement.classList.toggle('clicked');
-      this.otherCardText.nativeElement.classList.toggle('white');
-    }
-  }
+
 }
